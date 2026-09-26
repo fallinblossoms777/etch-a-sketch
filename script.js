5b433f60a1b9b4f558233701a7c3915bdd18d9d0
fallinblossoms777 <166082974+fallinblossoms777@users.noreply.github.com>
2026-09-26 02:17:01 -0300

    Remove unnecessary divs and add random colors on mouse hover

--- script.js
+++ script.js
@@ -5,3 +5,18 @@ for (let i = 0; i < TOTAL_DIVS; i++) {
     container.appendChild(div);
     div.classList.add('div');
 }
+
+// 256 × 256 × 256 = 16.777.216 -- MAXIMA CANTIDAD DE NUMEROS RGB (NUMEROS HEDECIMALES)
+const MAX_RGB_NUMBERS = 16777215 // Es 16777216, pero se le resta 1 porque se arranca desde 0.
+const divs = document.querySelectorAll(".div");
+
+divs.forEach((div) => {
+    div.addEventListener("mouseover", (event) => {
+    let randomHex = Math.floor(Math.random() * MAX_RGB_NUMBERS) // Math.random() - Genera numero decima entre 0 y 1
+        .toString(16) // Convierte los numeros en hexadecimal
+        .padStart(6, '0'); // Si da un hexadecimal con menos de 6 caracteres, se le agregan 0 para completar
+    
+    const target = event.target;
+    target.style.backgroundColor = "#" + randomHex;
+    });
+});
